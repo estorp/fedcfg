4f6b0e19363ae2899a0bb8c45ee581a03f21ea7e
pestor <paul.estor@dxc.com>
2019-07-17 21:22:21 +0800

    Added/Changes to the printout

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 private Hello name() {
     public void name() {
-        
+        print "hello world"
+    }
+    public static void name() {
+        System.err.println("NOTHING WRONG HERE. GET LOST");
     }
 }
